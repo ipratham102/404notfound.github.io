e548d00be4178e6f73df76c81067f9df59491a8a
Pratham Ahuja <102403321+ipratham102@users.noreply.github.com>
2022-04-09 18:13:10 +0530

    Add files via upload

---
+++ final front page/1.js
@@ -0,0 +1,11 @@
+function openFun(){
+    document.getElementById("menu").style.width="300px"
+    // document.getElementById("mainbox").style.marginLeft="300px"
+    // document.getElementById("mainbox").style.marginRight="30px"
+    // document.getElementById("mainbox").innerHTML="click on Cross Element and Close Menu";
+}
+function closeFun(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginLeft="0px";
+    document.getElementById("mainbox").style.innerHTML="&#9776; Open";
+}
